60604d686cfd19ef98341a5c94b8d58120e59ab8
Fernando Morais <fernandouitec@gmail.com>
2025-11-09 21:09:55 -0300

    first commit

---
+++ config/routes.php
@@ -0,0 +1,10 @@
+<?php
+
+use Slim\App;
+
+return function (App $app) {
+
+    //ROTAS DO WEB SITE
+    $app->get('/', '\App\Controller\HomeController:home');
+    $app->get('/{any}', '\App\Controller\HomeController:page');
+};
---
+++ config/settings.php
@@ -0,0 +1,17 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+// Timezone
+date_default_timezone_set('America/Sao_Paulo');
+
+//Definindo diretorio principal
+define('DIRETORIO_PRINCIPAL', 'crc');
+define('DIRETORIO_TEMPLATES', 'src/Views');
+// define('URL_BASE', 'http://localhost' . DIRETORIO_PRINCIPAL . '/');
+define('URL_BASE', 'https://crc.test/');
+
+define('SQL_DB_SERVER', 'localhost');
+define('SQL_DB_USER', 'root');
+define('SQL_DB_PASS', 'root');
+define('SQL_DB_DATABASE', 'db_crc');
